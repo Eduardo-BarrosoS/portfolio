8fe3770926898522e29d31a565272b3541cf9c82
Edaurdo Barroso <barrosoeduardo64@gmail.com>
2022-12-06 19:09:00 -0300

    Projects

---
+++ src/sections/Projects/styles.ts
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const Cards = styled.div`
+
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+    align-items: center;
+    row-gap: 1rem;
+
+`
